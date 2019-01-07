84e32739944a87b58a6b319bd1d0cad8fa551a47
gdin22 <1171341386@qq.com>
2019-01-07 10:47:05 +0800

    重写了headers

--- allasin.py
+++ allasin.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
         output_asin = deal.output_asin()    # 从爬取的信息和已发送的信息进行对比 把没有发送的发送出去
         deal.get_has_post()  # 获取已发送的asin 如果在新一轮的爬取中 没有跟卖了 就应该从已发送中取出
         text = '   '.join(output_asin)
-        with open('asin.text', 'w') as f:
+        with open('asin.txt', 'w') as f:
             f.write(text)
-        deal.has_post_asin()    # 把发送的asin保存到数据库中
+        deal.has_post_asin()    # 把已发送的asin保存到数据库中
         deal.del_each_asin_number()  # 删除掉全部爬取的数据 等待新一轮的爬取
         end = time.time()
         until_time = end-start
         print(until_time)
-        if until_time > 10800:
+        print('\a')
+        if until_time < 10800:
             time.sleep(10800 - until_time)
